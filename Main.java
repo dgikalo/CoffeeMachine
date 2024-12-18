764707464d38849b0d79a67147b7d7d6d41b4a84
Denis Gikalo <denis.gikalo@outlook.com>
2024-12-18 21:10:42 +0500

    Resource enum implemented and will be added; CoffeeMachine class implemented and will be added

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.Scanner;
 
 
@@ -10,6 +12,92 @@ enum MainMenuOption {
     TAKE,
     REMAINING,
     EXIT
+}
+
+
+enum Resource {
+    WATER,
+    MILK,
+    COFFEE_BEANS,
+    CUPS,
+    MONEY
+}
+
+
+final class CoffeeMachine {
+    private static EnumMap<Resource, Integer> resources;
+
+    public CoffeeMachine() {
+        resources = new EnumMap<>(Resource.class);
+        resources.put(Resource.WATER, 400);
+        resources.put(Resource.MILK, 540);
+        resources.put(Resource.COFFEE_BEANS, 120);
+        resources.put(Resource.CUPS, 9);
+        resources.put(Resource.MONEY, 550);
+    }
+
+    public Integer getWaterAmount() {
+        return resources.get(Resource.WATER);
+    }
+
+    public void updateWaterAmount(int value) {
+        resources.put(Resource.WATER, value);
+    }
+
+    public Integer getMilkAmount() {
+        return resources.get(Resource.MILK);
+    }
+
+    public void updateMilkAmount(int value) {
+        resources.put(Resource.MILK, value);
+    }
+
+    public Integer getCoffeeBeansAmount() {
+        return resources.get(Resource.COFFEE_BEANS);
+    }
+
+    public void updateCoffeeBeansAmount(int value) {
+        resources.put(Resource.COFFEE_BEANS, value);
+    }
+
+    public Integer getDisposableCupsNumber() {
+        return resources.get(Resource.CUPS);
+    }
+
+    public void updateDisposableCupsNumber(int value) {
+        resources.put(Resource.CUPS, value);
+    }
+
+    public Integer getMoneyAmount() {
+        return resources.get(Resource.MONEY);
+    }
+
+    public Integer updateMoneyAmount(int value) {
+        resources.put(Resource.MONEY, value);
+    }
+
+    public void displayStatus() {
+        System.out.println("The coffee machine has:");
+        System.out.printf("%d ml of water", getWaterAmount());
+        System.out.printf("%d ml of milk", getMilkAmount());
+        System.out.printf("%d g of coffee beans", getCoffeeBeansAmount());
+        System.out.printf("% disposable cups", getDisposableCupsNumber());
+        System.out.printf("$%d of money", getMoneyAmount());
+    }
+}
+
+
+final class MainMenuProcessor {
+
+    public static void processMainMenu(MainMenuOption option) {
+        switch (option) {
+            case BUY -> System.out.println("Buy");
+            case FILL -> System.out.println("Fill");
+            case TAKE -> System.out.println("Take");
+            case REMAINING -> System.out.println("Remaining");
+            case EXIT -> System.exit(0);
+        }
+    }
 }
 
 
@@ -41,6 +129,10 @@ public class Main {
 
             System.out.println(validatedValue);
             System.out.println();
+
+            if (validatedValue.equals(MainMenuOption.EXIT)) {
+                return;
+            }
         }
     }
 }
